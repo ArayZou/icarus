f8e2f36f6e0539d889665144f72e62eb135b1332
r_zou <r_zou@Ctrip.com>
2015-04-23 18:04:50 +0800

    share button

--- source/js/script.js
+++ source/js/script.js
@@ -1,63 +1,6 @@
 (function($){
   var toTop = $('#toTop').offset().top - $(window).height() + 20;
 
-  // Share
-  $('body').on('click', function(){
-    $('.article-share-box.on').removeClass('on');
-  }).on('click', '.article-share-link', function(e){
-    e.stopPropagation();
-
-    var $this = $(this),
-      url = $this.attr('data-url'),
-      encodedUrl = encodeURIComponent(url),
-      title = $this.attr('data-title'),
-      encodedTitle = encodeURIComponent(title),
-      id = 'article-share-box-' + $this.attr('data-id'),
-      offset = $this.offset();
-
-    if ($('#' + id).length){
-      var box = $('#' + id);
-
-      if (box.hasClass('on')){
-        box.removeClass('on');
-        return;
-      }
-    } else {
-      var html = [
-        '<div id="' + id + '" class="article-share-box">',
-          '<input class="article-share-input" value="' + url + '">',
-          '<div class="article-share-links">',
-            '<a href="http://service.weibo.com/share/share.php?title='+encodedTitle+'&url='+encodedUrl +'&ralateUid=1824444687&searchPic=true&style=number' +'" class="fa fa-weibo article-share-weibo" target="_blank" title="微博"></a>',
-            '<a href="https://twitter.com/intent/tweet?url=' + encodedUrl + '" class="fa fa-twitter article-share-twitter" target="_blank" title="Twitter"></a>',
-            '<a href="https://www.facebook.com/sharer.php?u=' + encodedUrl + '" class="fa fa-facebook article-share-facebook" target="_blank" title="Facebook"></a>',
-            '<a href="http://pinterest.com/pin/create/button/?url=' + encodedUrl + '" class="fa fa-pinterest article-share-pinterest" target="_blank" title="Pinterest"></a>',
-            '<a href="https://plus.google.com/share?url=' + encodedUrl + '" class="fa fa-google article-share-google" target="_blank" title="Google+"></a>',
-          '</div>',
-        '</div>'
-      ].join('');
-
-      var box = $(html);
-
-      $('body').append(box);
-    }
-
-    $('.article-share-box.on').hide();
-
-    box.css({
-      top: offset.top + 25,
-      left: offset.left
-    }).addClass('on');
-  }).on('click', '.article-share-box', function(e){
-    e.stopPropagation();
-  }).on('click', '.article-share-box-input', function(){
-    $(this).select();
-  }).on('click', '.article-share-box-link', function(e){
-    e.preventDefault();
-    e.stopPropagation();
-
-    window.open(this.href, 'article-share-box-window-' + Date.now(), 'width=500,height=450');
-  });
-
   // Caption
   $('.article-entry').each(function(i){
     $(this).find('img').each(function(){
@@ -103,7 +46,7 @@
       $('#toTop').css('right', 20);
     }
   }).on('click', '#toTop', function () {
-    $(document).scrollTop(0);
+    $('html,body').animate({scrollTop:0},300);
   });
 
 })(jQuery);
